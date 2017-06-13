6ab7f1d15438fff47f33deb3fa140def3adf8c0b
zach ma <zach.ma@centling.com>
2017-06-14 06:26:23 +0800

    LOG-3 添加spring boot hello world例子。由于在App的主启动类上面没有添加相关注解，导致没法启动。

--- src/main/java/com/zach/App.java
+++ src/main/java/com/zach/App.java
@@ -1,10 +1,22 @@
 package com.zach;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * Hello world!
+ * 在这里我们使用@SpringBootApplication指定这是一个 spring boot的应用程序.
  */
+//extends WebMvcConfigurerAdapter  http://blog.csdn.net/cloume/article/details/48439429
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /**
+         * 这是springloader的配置方式：-javaagent:.\lib\springloaded-1.2.4.RELEASE.jar -noverify
+         * @param args
+         */
+        /*
+		 * 在main方法进行启动我们的应用程序.
+		 */
+        SpringApplication.run(App.class, args);
     }
 }
