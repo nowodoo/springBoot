37b6a7e8ff4588c8d8c2fd2c57354dda353e4269
zach ma <zach.ma@centling.com>
2017-06-14 06:42:47 +0800

    LOG-3 class2 使用默认的jackson返回json数据。

--- src/main/java/com/zach/HelloController.java
+++ src/main/java/com/zach/HelloController.java
@@ -1,7 +1,10 @@
 package com.zach;
 
+import com.zach.domain.Demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * 在这里我们使用RestController  （等待于 @Controller 和 @RequestBody）
@@ -18,4 +21,18 @@ public class HelloController {
 	public String hello(){
 		return "hello";
 	}
+
+    /**
+     * 测试返回json
+     * @return
+     */
+    @RequestMapping("/getDemo")
+    public Demo getDemo(){
+        Demo demo = new Demo();
+        demo.setId(1);
+        demo.setName("张三");
+        demo.setCreateTime(new Date());
+        demo.setRemarks("这是备注信息");
+        return demo;
+    }
 }
